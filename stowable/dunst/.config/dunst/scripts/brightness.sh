443087936bcaf07310fd840c068909e09a5aaab4
aurum77 <58880138+aurum77@users.noreply.github.com>
2022-05-08 22:41:30 +0300

    add dunst config files and scripts

---
+++ stowable/dunst/.config/dunst/scripts/brightness.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# You can call this script like this:
+# $ ./brightness.sh up
+# $ ./brightness.sh down
+
+notify_id=817
+
+function send_notification {
+  brightnow=$(brightnessctl get)
+  brightmax=$(brightnessctl max)
+
+  brightpercent=$(awk "BEGIN {printf ${brightnow}/${brightmax}*100}" | cut -d. -f1)
+
+  dunstify -r $notify_id -u low \
+    -h int:value:"$brightpercent" "brightness:${brightpercent}%"
+}
+
+case $1 in
+  up)
+    # increase the backlight by 10
+    brightnessctl set +10
+    send_notification
+    ;;
+  down)
+    # decrease the backlight by 10
+    brightnessctl set 10-
+    send_notification
+    ;;
+esac
