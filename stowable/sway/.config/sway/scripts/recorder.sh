bfe1872244ec98347b0bba6e5ee2e8eebb8c06d1
aurum77 <58880138+aurum77@users.noreply.github.com>
2022-05-08 22:56:01 +0300

    fix wf-recorder script

--- stowable/sway/.config/sway/scripts/recorder.sh
+++ stowable/sway/.config/sway/scripts/recorder.sh
@@ -1,11 +1,20 @@
 #!/bin/bash 
 
 pid=`pgrep wf-recorder`
-status=$?
+status_recorder=$?
 
-if [ $status != 0 ] 
-then 
-  wf-recorder -g "$(slurp)" -a alsa_output.pci-0000_05_00.6.analog-stereo -f $(xdg-user-dir VIDEOS)/$(date +'recording_%Y-%m-%d-%H%M%S.mp4');
-else 
+if [[ $status_recorder == 0 ]];
+then
   pkill --signal SIGINT wf-recorder
+  exit
+fi;
+
+slurp=$(slurp)
+status_slurp=$?
+
+if [[ $status_slurp == 0 ]];
+then 
+  wf-recorder -g "$slurp" -a alsa_output.pci-0000_05_00.6.analog-stereo -f $(xdg-user-dir VIDEOS)/$(date +'recording_%Y-%m-%d-%H%M%S.mp4');
+else
+  exit
 fi;
