bb9a2cc1ac0abb64966ea54b6157d0fcabdbce47
aurum77 <58880138+aurum77@users.noreply.github.com>
2022-07-08 20:45:10 +0300

    move sound script to swaywm folder

---
+++ stowable/sway/.config/sway/scripts/sound.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# You can call this script like this:
+# $ ./sound.sh up
+# $ ./sound.sh down
+# $ ./sound.sh mute
+
+soundlimit=100
+
+function get_volume {
+  pactl get-sink-volume @DEFAULT_SINK@ | awk '{print $5}'
+}
+
+function get_muted {
+  pactl get-sink-mute @DEFAULT_SINK@ | awk '{print $2}'
+}
+
+function get_volume_level {
+  volume=`get_volume`
+  echo ${volume%\%}
+}
+
+function exit_if_muted {
+  if [[ $(get_muted) == 'yes' ]]; then
+    exit
+  fi
+}
+
+case $1 in
+  up)
+    exit_if_muted
+    if [[ $(get_volume_level) -lt $soundlimit ]]; then
+      pactl set-sink-volume @DEFAULT_SINK@ +5%
+    fi
+    ;;
+  down)
+    exit_if_muted
+    pactl set-sink-volume @DEFAULT_SINK@ -5%
+    ;;
+  mute)
+    pactl set-sink-mute @DEFAULT_SINK@ toggle
+    ;;
+esac
