6e5df6a4e398f2164f323ea1fe27f30ac052e2de
aurum77 <58880138+aurum77@users.noreply.github.com>
2022-12-31 01:07:08 +0300

    swaywm changes

---
+++ stowable/wofi/.config/wofi/scripts/wofi-power.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+entries="Shutdown\nReboot\nLogout\nHibernate\nSuspend\nLock"
+
+selected=$(echo -e $entries | wofi --show dmenu -i --lines=7 --width=15% --hide-scroll --cache-file=/dev/null --prompt="Power Options" | awk '{print tolower($1)}')
+
+case $selected in
+  shutdown)
+    exec systemctl poweroff -i;;
+  hibernate)
+    exec systemctl hibernate;;
+  suspend)
+    exec systemctl suspend;;
+  reboot)
+    exec systemctl reboot;;
+  logout)
+    exec loginctl terminate-session $XDG_SESSION_ID;;
+  lock)
+    exec swaylock -i $HOME/pics/wall.jpg;;
+esac
