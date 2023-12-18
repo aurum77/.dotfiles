2c7f01d33c52feee083cfcd7ceced5da7000f856
aurum77 <58880138+aurum77@users.noreply.github.com>
2023-12-19 00:14:32 +0300

    back to arch

--- fonts.sh
+++ fonts.sh
@@ -1,7 +1,9 @@
+rm -rf ~/.fonts
 # Get the latest fonts
 echo "Getting nerd fonts"
 wget -q --show-progress $(curl -s https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest |  jq --raw-output '.assets[] | select(.name | test("^(NerdFontsSymbolsOnly).zip$")) .browser_download_url') -O /tmp/ttf-symbols.zip
 
 echo "Installing fonts"
+mkdir ~/.fonts
 unzip -q /tmp/ttf-symbols.zip -d /tmp/ttf-symbols/
 mv /tmp/ttf-symbols/SymbolsNerdFont-Regular.ttf $HOME/.fonts/
