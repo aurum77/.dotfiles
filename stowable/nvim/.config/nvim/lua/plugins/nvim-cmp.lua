5ae3dfa0365b0e8a098e36d35ec997e45c00fc4a
aurum77 <58880138+aurum77@users.noreply.github.com>
2025-12-28 14:47:58 +0300

    fix nvim cmp colors

--- stowable/nvim/.config/nvim/lua/plugins/nvim-cmp.lua
+++ stowable/nvim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -3,16 +3,33 @@ return {
 	event = { "InsertEnter", "CmdlineEnter" },
 	config = function()
 		local cmp = require("cmp")
-
-		local check_backspace = function()
-			local col = vim.fn.col(".") - 1
-			return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
-		end
-
-		local function has_words_before()
-			local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-			return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-		end
+		local kind_icons = {
+			Text = "󰉿",
+			Method = "󰆧",
+			Function = "󰊕",
+			Constructor = "",
+			Field = "󰜢",
+			Variable = "󰀫",
+			Class = "󰠱",
+			Interface = "",
+			Module = "",
+			Property = "󰜢",
+			Unit = "󰑭",
+			Value = "󰎠",
+			Enum = "",
+			Keyword = "󰌋",
+			Snippet = "",
+			Color = "󰏘",
+			File = "󰈙",
+			Reference = "󰈇",
+			Folder = "󰉋",
+			EnumMember = "",
+			Constant = "󰏿",
+			Struct = "󰙅",
+			Event = "",
+			Operator = "󰆕",
+			TypeParameter = "",
+		}
 
 		cmp.setup({
 			snippet = {
@@ -28,24 +45,25 @@ return {
 				entries = { name = "custom", selection_order = "near_cursor" },
 			},
 			formatting = {
-				format = require("lspkind").cmp_format({
-					mode = "text",
-					menu = {
+				fields = { "abbr", "kind", "menu" },
+				format = function(entry, vim_item)
+					-- Kind icons
+					vim_item.kind = string.format("%s %s", kind_icons[vim_item.kind], vim_item.kind)
+					vim_item.menu = ({
 						nvim_lsp = "[LSP]",
 						luasnip = "[LuaSnip]",
 						path = "[Path]",
 						buffer = "[Buffer]",
 						emoji = "[Emoji]",
-						nvim_lsp_signature_help = "[Sign]",
+						nvim_lsp_signature_help = "[Signature]",
 						nvim_lua = "[API]",
-					},
-				}),
+					})[entry.source.name]
+					return vim_item
+				end,
 			},
 			mapping = cmp.mapping.preset.insert({
 				["<C-b>"] = cmp.mapping.scroll_docs(-4),
 				["<C-f>"] = cmp.mapping.scroll_docs(4),
-				-- ["<Tab>"] = cmp.mapping.select_next_item(),
-				-- ["<S-Tab>"] = cmp.mapping.select_prev_item(),
 				["<Tab>"] = cmp.mapping(function(fallback)
 					if cmp.visible() then
 						cmp.select_next_item()
@@ -99,7 +117,6 @@ return {
 			}),
 		})
 
-		-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
 		cmp.setup.cmdline({ "/", "?" }, {
 			mapping = cmp.mapping.preset.cmdline(),
 			sources = {
@@ -107,7 +124,6 @@ return {
 			},
 		})
 
-		-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
 		cmp.setup.cmdline(":", {
 			mapping = cmp.mapping.preset.cmdline(),
 			sources = cmp.config.sources({
