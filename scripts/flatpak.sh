6fbe47adf9c7a1914c186ff3216dfcd90ea351a6
aurum77 <58880138+aurum77@users.noreply.github.com>
2026-02-04 21:34:00 +0300

    post reinstall changes

--- scripts/flatpak.sh
+++ scripts/flatpak.sh
@@ -1,17 +1,17 @@
-flatpak install -y --noninteractive flathub net.lutris.Lutris
-flatpak install -y --noninteractive flathub com.heroicgameslauncher.hgl
-flatpak install -y --noninteractive flathub com.valvesoftware.Steam
+# flatpak install -y --noninteractive flathub net.lutris.Lutris
+# flatpak install -y --noninteractive flathub com.heroicgameslauncher.hgl
+# flatpak install -y --noninteractive flathub com.valvesoftware.Steam
 flatpak install -y --noninteractive flathub org.deluge_torrent.deluge
 flatpak install -y --noninteractive flathub io.dbeaver.DBeaverCommunity
 flatpak install -y --noninteractive flathub com.stremio.Stremio
 flatpak install -y --noninteractive flathub com.protonvpn.www
-flatpak install -y --noninteractive flathub org.freedesktop.Platform.VulkanLayer.MangoHud 23.08
-flatpak install -y --noninteractive flathub org.freedesktop.Platform.VulkanLayer.MangoHud 25.08
+# flatpak install -y --noninteractive flathub org.freedesktop.Platform.VulkanLayer.MangoHud 23.08
+# flatpak install -y --noninteractive flathub org.freedesktop.Platform.VulkanLayer.MangoHud 25.08
 
 flatpak override --user --filesystem=$HOME/.themes/
 flatpak override --user --filesystem=$HOME/.icons/
 
-flatpak override --user --nofilesystem=~/Games/Heroic com.heroicgameslauncher.hgl
-flatpak override --user --filesystem=$HOME/games/heroic:create com.heroicgameslauncher.hgl
+# flatpak override --user --nofilesystem=~/Games/Heroic com.heroicgameslauncher.hgl
+# flatpak override --user --filesystem=$HOME/games/heroic:create com.heroicgameslauncher.hgl
 
-flatpak override --user --filesystem=xdg-config/MangoHud:ro
+# flatpak override --user --filesystem=xdg-config/MangoHud:ro
--- stowable/nvim/.config/nvim/lua/core/theme.lua
+++ stowable/nvim/.config/nvim/lua/core/theme.lua
@@ -38,6 +38,10 @@ vim.api.nvim_set_hl(0, "TelescopeResultsBorder", { fg = gruvbox.light0 })
 vim.api.nvim_set_hl(0, "TelescopeResultsTitle", { fg = gruvbox.light1 })
 vim.api.nvim_set_hl(0, "TelescopeSelectionCaret", { fg = gruvbox.bright_orange })
 -- Telescope
+-- TreesitterContext
+vim.api.nvim_set_hl(0, "TreesitterContext", { bg = gruvbox.dark1, bold = true })
+vim.api.nvim_set_hl(0, "TreesitterContextLineNumber", { bg = gruvbox.dark1, fg = gruvbox.bright_yellow, bold = true })
+-- TreesitterContext
 -- Trouble
 vim.api.nvim_set_hl(0, "TroubleFoldIcon", { bg = "NONE", fg = gruvbox.bright_yellow })
 vim.api.nvim_set_hl(0, "TroubleSignError", { bg = "NONE", fg = gruvbox.bright_red })
